15c68b101e11d4487fec36073e9069028d103d4f
Berend Tober <btober@laptop.btober.net>
2017-08-05 13:36:38 -0400

    Added configuration dialog. Added query execution.

--- fairwinds-gui.py
+++ fairwinds-gui.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+import os
 from tkinter import *
+
+
+def executesql(sql):
+    cmd = ('psql -q <<< "\\set VERBOSITY terse\n\\set QUIET on\n{}"'.format(sql))
+    os.system(cmd)
 
 
 def center_window(window, width=300, height=200):
@@ -17,9 +23,11 @@ def center_window(window, width=300, height=200):
 
 class LabelledEntry:
     """ defines a class included both a label and an entry """
-    def __init__(self, owner, text=""):
+    def __init__(self, owner, text="", value="", show=""):
         self.frame = Frame(owner)
-        self.entry = Entry(self.frame)
+        self.entry = Entry(self.frame, show=show)
+        self.entry.delete(0, END)
+        self.entry.insert(0, value)
         self.label = Label(self.frame, text=text)
 
     def pack(self):
@@ -29,6 +37,9 @@ class LabelledEntry:
 
     def get(self):
         return self.entry.get()
+
+    def focus_set(self):
+        self.entry.focus_set()
 
 
 def parallelize(dictionary):
@@ -42,14 +53,74 @@ def parallelize(dictionary):
     return column_list[2:], value_list[2:]
 
 
+class Configure(Toplevel):
+
+    def __init__(self):
+        def buildsql():
+            """ constructs a sql insert statement """
+
+            if host_name_entry.get():
+                os.environ["PGHOST"] = host_name_entry.get()
+            if dbname_name_entry.get():
+                os.environ["PGDATABASE"] = dbname_name_entry.get()
+            if fairian_entry.get():
+                os.environ["PGUSER"] = fairian_entry.get()
+            if password_entry.get():
+                os.environ["PGPASSWORD"] = password_entry.get()
+
+
+        Toplevel.__init__(self, root)
+        self.transient(root)
+        self.title("Configuration")
+        self.geometry("250x100")
+        host_name_entry = LabelledEntry(self, text="Host", value=os.environ.get("PGHOST",""))
+        host_name_entry.pack()
+        dbname_name_entry = LabelledEntry(self, text="Data Base", value=os.environ.get("PGDATABASE",""))
+        dbname_name_entry.pack()
+        fairian_entry = LabelledEntry(self, text="Fairian Name", value=os.environ.get("PGUSER",""))
+        fairian_entry.pack()
+        fairian_entry.focus_set()
+        password_entry = LabelledEntry(self, text="Password", value=os.environ.get("PGPASSWORD",""), show="*")
+        password_entry.pack()
+
+        button_frame = Frame(self, borderwidth=3)
+        button_frame.pack(anchor="s", side=BOTTOM)
+
+        Button(button_frame, text="Exit", command=self.destroy).pack(side=LEFT, anchor="center")
+        Button(button_frame, text="Ok", command=buildsql).pack(side=LEFT, anchor="center")
+
+
 class CreateFairian(Toplevel):
     def __init__(self):
+        def buildsql():
+            """ constructs a sql insert statement """
+
+            column_values = {}
+
+            if fairian_entry.get():
+                column_values["fairian_name"] = fairian_entry.get()
+            if password_entry.get():
+                column_values["passwd"] = password_entry.get()
+            if email_address_entry.get():
+                column_values["email_address"] = email_address_entry.get()
+
+            column_list, value_list = parallelize(column_values)
+
+            fairian_name = os.environ.get("PGUSER", "")
+            os.environ["PGUSER"] = "anonymous"
+            executesql("insert into fairian ({}) values ({});".format(
+                            column_list,
+                            value_list
+                            ))
+            os.environ["PGUSER"] = fairian_name
+
         Toplevel.__init__(self, root)
         self.transient(root)
         self.title("Create Fairian")
         self.geometry("250x100")
-        fairian_entry = LabelledEntry(self, text="Fairian Name")
+        fairian_entry = LabelledEntry(self, text="Fairian Name", value=os.environ["PGUSER"])
         fairian_entry.pack()
+        fairian_entry.focus_set()
         password_entry = LabelledEntry(self, text="Password")
         password_entry.pack()
         email_address_entry = LabelledEntry(self, text="Email Address")
@@ -59,7 +130,7 @@ class CreateFairian(Toplevel):
         button_frame.pack(anchor="s", side=BOTTOM)
 
         Button(button_frame, text="Exit", command=self.destroy).pack(side=LEFT, anchor="center")
-        Button(button_frame, text="Next", command=None).pack(side=LEFT, anchor="center")
+        Button(button_frame, text="Next", command=buildsql).pack(side=LEFT, anchor="center")
 
 
 class LaborContracts(Toplevel):
@@ -146,7 +217,7 @@ class TradeAction(Toplevel):
 
             column_list, value_list = parallelize(column_values)
 
-            print("insert into {}_{} ({}) values ({});".format(
+            executesql("insert into {}_{} ({}) values ({});".format(
                             markets[selected_market.get()-1][1],
                             sides[selected_side.get()-1][1],
                             column_list,
@@ -203,6 +274,7 @@ class Application(Tk):
 
         main_menu.add_cascade(label="Actions", menu=action_menu)
 
+        action_menu.add_command(label="Configuration", command=Configure)
         action_menu.add_command(label="Create Fairian", command=CreateFairian)
         action_menu.add_command(label="Trade", command=TradeAction)
         action_menu.add_command(label="Manage Labor Contracts", command=LaborContracts)
